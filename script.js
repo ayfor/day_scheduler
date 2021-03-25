7e04ac28f521b3237757f7c1bad7fb1892220dc7
ayfor <josh.a.stubbington@gmail.com>
2021-03-25 14:48:42 -0400

    added color coding to time block with moment

--- script.js
+++ script.js
@@ -5,55 +5,55 @@ var currentDateElement = $('#currentDay')
 //Define array to display time blocks
 var timeblockArray = [
     {
-        time: "9:00",
+        time: "9:00am",
         localStorageIndex:0,
         html:'<div class="time-block row"><div class="hour">9:00am</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">9:00am</div><textarea data-index=0></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "10:00",
+        time: "10:00am",
         localStorageIndex:1,
         html:'<div class="time-block row"><div class="hour">10:00am</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">10:00am</div><textarea data-index=1></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "11:00",
+        time: "11:00am",
         localStorageIndex:2,
         html:'<div class="time-block row"><div class="hour">11:00am</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">11:00am</div><textarea data-index=2></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "12:00",
+        time: "12:00pm",
         localStorageIndex:3,
         html:'<div class="time-block row"><div class="hour">12:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">12:00pm</div><textarea data-index=3></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "13:00",
+        time: "1:00pm",
         localStorageIndex:4,
         html:'<div class="time-block row"><div class="hour">1:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">1:00pm</div><textarea data-index=4></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "14:00",
+        time: "2:00pm",
         localStorageIndex:5,
         html:'<div class="time-block row"><div class="hour">2:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">2:00pm</div><textarea data-index=5></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "15:00",
+        time: "3:00pm",
         localStorageIndex:6,
         html:'<div class="time-block row"><div class="hour">3:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">3:00pm</div><textarea data-index=6></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "16:00",
+        time: "4:00pm",
         localStorageIndex:7,
         html:'<div class="time-block row"><div class="hour">4:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">4:00pm</div><textarea data-index=7></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
     },
     {
-        time: "17:00",
+        time: "5:00pm",
         localStorageIndex:8,
         html:'<div class="time-block row"><div class="hour">5:00pm</div><textarea></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>',  
         domObject: $('<div class="time-block row"><div class="hour">5:00pm</div><textarea data-index=8></textarea><button class="saveBtn"><i class="fas fa-save"></i></button></div>)'),
@@ -62,25 +62,21 @@ var timeblockArray = [
 ]
 
 function importTimeBlocks(){
-    //Check if there is content in local storage
-    if(!(localStorage.length===0)){
-
-    }
-
 
     for (let index = 0; index < timeblockArray.length; index++) {
-        var currentEntry = ''
+        const timeblockArrayElement = timeblockArray[index];
+        var currentEntry = '';
         //Check if there is existing content in local storage
         for (let j = 0; j < localStorage.length; j++) {
             const element = localStorage.getItem(j);
             if(element){
-                if(index===j){
+                if(index===timeblockArrayElement.localStorageIndex){
                  currentEntry = element;   
                 }  
             }
         }
         
-        const timeblockArrayElement = timeblockArray[index];
+        
         timeblockArrayElement.domObject.children('textarea').text(currentEntry);
         timeblockContainerElement.append(timeblockArrayElement.domObject);
     }
@@ -115,8 +111,42 @@ function displayDate(){
     currentDateElement.text(moment().format('dddd, MMMM Do YYYY'));
 }
 
+function compareTimeBlocks(currentTime){
+    timeblockArray.forEach(element => {
+        let timeblockTime = moment(element.time,"h:mma");
+        let timeblockHour = timeblockTime.format("H");
+
+        //Get handle to jQuery DOM object text area
+        let display = element.domObject.children('textarea');
+        
+        //Check if current timeblock is in the past
+        if(+currentTime>+timeblockHour){
+            display.removeClass("present");
+            display.addClass("past");
+
+        }else if(+currentTime<+timeblockHour){
+            display.removeClass("past")
+            display.addClass("future");
+        }else{
+            display.removeClass("future")
+            display.addClass("present")
+        }
+    });
+}
+
+function colorCodeTimeBlocks(){
+    var timeInterval = setInterval(function (){
+        let currentTime = moment().format("H");
+        compareTimeBlocks(currentTime); 
+    }, 1000)
+    
+}
+
+
+
 
 timeblockContainerElement.on('click', '.saveBtn',handleSaveItem);
 
 importTimeBlocks();
 displayDate();
+colorCodeTimeBlocks();
